e8b82d98893d5691cc2dc42e8ca75926cc20afa1
Cliff Moon <cliff@leaninto.it>
2015-09-12 21:49:34 -0700

    it's working

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot beavis.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.0.0"]]}}
+  :plugins [[s3-wagon-private "1.1.2"]]
+  :repositories [["snapshots" {:url "s3p://opsee-maven-snapshots/snapshot"
+                               :username :env
+                               :passphrase :env}]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [io.nodyn/nodyn "0.1.1-SNAPSHOT"]
+                 [riemann "0.2.10"]])
--- src/beavis/core.clj
+++ src/beavis/core.clj
@@ -1,7 +1,16 @@
 (ns beavis.core
-  (:gen-class))
+  (:gen-class)
+  (:import (io.nodyn.runtime RuntimeFactory RuntimeFactory$RuntimeType NodynConfig)
+           (io.nodyn NoOpExitHandler)))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+
+
+(defn -main [& args]
+  (let [factory (RuntimeFactory/init (-> (Object.) .getClass .getClassLoader) RuntimeFactory$RuntimeType/DYNJS)
+        config (NodynConfig. (into-array ["-e" (str "var slate = require('./js/slate/index');"
+                                                    "var example = require('./js/slate/src/example');"
+                                                    "var result = slate.testCheck(example);"
+                                                    "console.log(result);")]))
+        nodyn (.newRuntime factory config)]
+    (.setExitHandler nodyn (NoOpExitHandler.))
+    (.run nodyn)))
