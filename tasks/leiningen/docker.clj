301b2fecc7f09e32d73796c93b9ed94396d9ec67
Cliff Moon <cliff@leaninto.it>
2015-09-19 21:34:49 -0700

    ARE YOU THREATENING ME

--- tasks/leiningen/docker.clj
+++ tasks/leiningen/docker.clj
@@ -8,8 +8,8 @@
       "Build docker image"
       [project]
       (let [jar-path (get-jar-filename project :standalone)]
-           (sh "cp" jar-path "docker/lib/bartnet.jar")
-           (let [docker (sh "docker" "build" "-t" "quay.io/opsee/bartnet" "docker")
+           (sh "cp" jar-path "docker/lib/beavis.jar")
+           (let [docker (sh "docker" "build" "-t" "quay.io/opsee/beavis" "docker")
                  git-ref (sh "git" "rev-parse" "--verify" "HEAD")]
                 (if-not (= 0 (:exit git-ref))
                         (println (:err git-ref))
@@ -17,4 +17,4 @@
                           (println (:out docker))
                           (if-not (blank? (:err docker))
                                   (println "Build error: " (:err docker))
-                                  (sh "docker" "build" "-t" (str "quay.io/opsee/bartnet:" (trim (:out git-ref))) "docker")))))))
+                                  (sh "docker" "build" "-t" (str "quay.io/opsee/beavis:" (trim (:out git-ref))) "docker")))))))
