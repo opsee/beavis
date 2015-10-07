8c694ee5d136854b4c988db6f840f23beb350174
Greg Poirier <grep@binary-snobbery.com>
2015-10-07 13:32:52 -0700

    Add CheckResult fixture generation functions

--- test/beavis/fixtures.clj
+++ test/beavis/fixtures.clj
@@ -1,5 +1,7 @@
 (ns beavis.fixtures
-  (:require [beavis.sql :as sql]))
+  (:require [beavis.sql :as sql]
+            [beavis.protobuilder :refer :all])
+  (:import (co.opsee.proto Timestamp Target CheckResult HttpResponse Any CheckResponse)))
 
 
 
@@ -40,3 +42,60 @@
                                        :check_id "poop"
                                        :type "email"
                                        :value "poooooooooooo"}))
+
+(defn passing-response ^Any []
+  (-> (Any/newBuilder)
+      (.setTypeUrl "HttpResponse")
+      (.setValue (-> (HttpResponse/newBuilder)
+                     (.setBody "OK")
+                     (.setHost "hostname")
+                     (.setCode 200)
+                     .build
+                     .toByteString))
+      .build))
+
+(defn failing-response ^Any []
+  (-> (Any/newBuilder)
+      (.setTypeUrl "HttpResponse")
+      (.setValue (-> (HttpResponse/newBuilder)
+                     (.setBody "NOT OK")
+                     (.setHost "hostname")
+                     (.setCode 404)
+                     .build
+                     .toByteString))
+      .build))
+
+(defn check-response [index passing]
+  (let [address (str "192.168.0." index)
+        name (str "instance" index)
+        id (str "i-" (apply str (repeat 8 index)))]
+    (-> (CheckResponse/newBuilder)
+        (.setTarget (-> (Target/newBuilder)
+                        (.setAddress address)
+                        (.setName name)
+                        (.setId id)
+                        (.setType "instance")
+                        .build))
+        (.setResponse (if passing (passing-response) (failing-response)))
+        .build)))
+
+(defn check-result [num-responses passing-count check-index]
+  "check-result will produce a CheckResult object. It prepopulates
+  with a security group target that yields multiple responses. The
+  result will have num-responses associated responses. The check
+  index argument will allow you to create multiple successive events."
+  (-> (CheckResult/newBuilder)
+      (.setCheckId "check_id")
+      (.setTarget (-> (Target/newBuilder)
+                      (.setName "sg")
+                      (.setType "sg")
+                      (.setId "sg-sgsgsg")
+                      .build))
+      (.setTimestamp (-> (Timestamp/newBuilder)
+                         (.setSeconds (* check-index 30))
+                         .build))
+      (.addAllResponses (conj (apply vec
+                                     (map #(check-response % true) (range 0 passing-count))
+                                     (map #(check-response % false) (range 0 (- num-responses passing-count))))))
+      .build
+      ))
