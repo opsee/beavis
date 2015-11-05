9bde85392df325263ed54eabed5f09f690674a1b
Cliff Moon <cliff@leaninto.it>
2015-11-04 16:12:14 -0800

    revert the channel id until I figure something out that doesnt suck donkey dick

--- src/beavis/consumer.clj
+++ src/beavis/consumer.clj
@@ -52,7 +52,7 @@
     (start-producer! [_ next]
       (let [lookup (nsq-lookup (:lookup nsq-config) (:produce nsq-config))
             topic "_.results"
-            channel-id (:channel-id nsq-config)]
+            channel-id (str (java.util.UUID/randomUUID))]
         (log/info "channel" channel-id)
         (try
           (reset! consumer
