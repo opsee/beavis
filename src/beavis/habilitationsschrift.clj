ebe3ef5aec307acc163edebd18c264c11bdd97d3
Greg Poirier <grep@binary-snobbery.com>
2015-11-23 20:14:07 -0800

    Add flap detection to the riemann config

--- src/beavis/habilitationsschrift.clj
+++ src/beavis/habilitationsschrift.clj
@@ -35,7 +35,7 @@
 
 (defn delete-all-results [deletions]
   (doseq [[check-id customer-id] deletions]
-    (let [results (query {:service check-id
+    (let [results (query {:service     check-id
                           :customer_id customer-id})]
       (doseq [event results]
         (delete event)))))
@@ -44,7 +44,7 @@
   "In a container, we use /etc/beavis/riemann_config.clj, but when testing
   or running locally, we use it from the classpath."
   (let [config-file "riemann_config.clj"
-        etc-config  (str "/beavis/etc/" config-file)
+        etc-config (str "/beavis/etc/" config-file)
         resource-config (.getPath (io/resource config-file))]
     (if (-> etc-config (io/file) (.exists))
       etc-config
@@ -52,29 +52,48 @@
         (log/info "Unable to locate Riemann config in /beavis/etc")
         resource-config))))
 
+(defn set-response-state [event]
+  (let [mutated (assoc event :state (or (:passing event) false))]
+    mutated))
+
+(defn set-result-state [event]
+  (let [passing (every? #(= (:state %) true) (:responses event))
+        mutated (assoc event :passing passing :state passing)]
+    mutated))
+
+(defn set-error-state [event]
+  (assoc event :passing false))
+
+(defn set-state [event]
+  (cond (:responses event) (set-result-state event)
+        (:response event) (set-response-state event)
+        (:error event) (set-error-state event)
+        :else nil))
+
 (defn to-event [result response]
-  (assoc response
-    :host (or (get-in response [:target :id]) (get-in response [:target :name]))
-    :service (:check_id result)
-    :customer_id (:customer_id result)
-    :check_id (:check_id result)
-    :time (:timestamp result)))
+  (let [response-with-state (set-state response)]
+    (assoc response-with-state
+      :host (or (get-in response-with-state [:target :id]) (get-in response-with-state [:target :name]))
+      :service (:check_id result)
+      :customer_id (:customer_id result)
+      :check_id (:check_id result)
+      :time (:timestamp result))))
 
 (defn to-riemann-event [result-pb]
-    (let [result (binding [proto/formatter time-formatter] (-> result-pb
-                                                               (proto/proto->hash)
-                                                               (keywordize-keys)))
-          event (assoc result :responses (map #(assoc (to-event result %) :type "response")
-                                              (:responses result))
-                              :type "result")
-          mutated (to-event event event)]
-      mutated))
+  (let [result (binding [proto/formatter time-formatter] (-> result-pb
+                                                             (proto/proto->hash)
+                                                             (keywordize-keys)))
+        event (assoc result :responses (map #(assoc (to-event result %) :type "response")
+                                            (:responses result))
+                            :type "result")
+        mutated (to-event event event)]
+    mutated))
 
 (defn stream-and-return [event]
   (riemann.core/stream! @core event)
   (index/lookup (:index @core) (:host event) (:service event)))
 
-(def  ^:dynamic next-stage-fn)
+(def ^:dynamic next-stage-fn)
 
 (defn handle-event [result next]
   "Handle Results and Responses separately in the same Riemann core.
@@ -106,7 +125,27 @@
   (let [event (to-riemann-event result)
         responses (map stream-and-return (:responses event))]
     (binding [next-stage-fn next]
+      (doall responses)
+      (log/debug event)
       (stream-and-return (assoc event :responses responses)))))
+
+(defn kill-riemann-tasks []
+  (let [tasks riemann.time/tasks
+        tp @riemann.time/threadpool]
+    ;; Immediately prevent any additional tasks from executing.
+    (for [t (seq tasks)]
+      (riemann.time/cancel t))
+    ;; Interrupt any sleeping threads so that they may exit if they can--otherwise we will end up
+    ;; leaking Threads. If they don't ever exit, then this is a real problem.
+    ;; TODO(greg): Figure out if this actually does anything.
+    (for [^Thread t (seq tp)]
+      (try
+        (.interrupt t)
+        (catch InterruptedException e
+          (log/debug e "caught InterruptedException")))))
+  ;; This is probably dangerous and will cause us to leak tasks, but it's the only way that we can get riemann.time to
+  ;; stop and "cleanly" reset its state.
+  (reset! riemann.time/threadpool []))
 
 (defn riemann-stage []
   (reify
@@ -120,10 +159,11 @@
         (riemann.config/start!)
         nil))
     (stop-stage! [_]
+      "Do not call this if you are not shutting down beavis entirely."
+      (kill-riemann-tasks)
       (riemann.time/stop!)
       (riemann.config/stop!)
       nil)
-
     StreamStage
     (submit [_ work next]
       (handle-event work next))))
--- src/riemann_config.clj
+++ src/riemann_config.clj
@@ -1,40 +1,49 @@
 (require '[riemann.streams :refer :all]
+         '[riemann.index :refer [lookup]]
          '[riemann.core :as core]
          '[beavis.habilitationsschrift :refer [next-stage-fn]]
          '[clojure.tools.logging :as log])
-
-(defn set-response-state [event]
-  (let [mutated (assoc event :state (:passing event))]
-    mutated))
-
-(defn set-result-state [event]
-  (let [passing (every? #(= (:passing %) true) (:responses event))
-        mutated (assoc event :passing passing :state passing)]
-    mutated))
-
-(defn set-error-state [event]
-  (assoc event :passing false))
-
-(defn set-state [& children]
-  (fn [event]
-    (cond (:responses event) (call-rescue (set-result-state event) children)
-          (:response event) (call-rescue (set-response-state event) children)
-          (:error event) (call-rescue (set-error-state event) children)
-          :else nil)))
 
 (defn pass-to-next-stage [& children]
   (fn [event]
     (next-stage-fn event)
     (call-rescue event children)))
 
+(defn is-result [& children]
+  (fn [event]
+    (when (contains? event :responses)
+      (call-rescue event children))))
+
+(defn ignore-old-events [index & children]
+  (fn [event]
+    (let [indexed (lookup index (:host event) (:service event))]
+      (if indexed
+        (when (> (:time event) (:time indexed))
+          (call-rescue event children))
+        (call-rescue event children)))))
+
+(defn not-indexed [index & children]
+  (fn [event]
+    (let [indexed (lookup index (:host event) (:service event))]
+      (when (nil? indexed)
+        (call-rescue event children)))))
+
 (let [index (core/wrap-index (index))]
   ;; This stream must be, in total, synchronous. Any asynchronous operations
   ;; must happen in another stream. This stream only updates the in-memory
   ;; state so that after a call to stream!, we can immediately query the
-  ;; index to get the state of the mutated event.
+  ;; index to get the state of the mutated event. -greg
   (streams
     (by [:host :service]
-        (set-state
-          index
-          (changed :state
-                   (pass-to-next-stage))))))
+        (not-indexed index
+                     index)
+        ;; The timestamp for each response and result should be monotonically increasing.
+        ;; If we see an older timestamp for any of these, then it is possible out-of-order
+        ;; delivery, delayed delivery, or duplicate delivery. In any regard, let's simply
+        ;; drop it on the floor. -greg
+        (ignore-old-events index
+                           ;; three consecutive failures indicates a non-flapping state.
+                           (stable 90 :state
+                                   index
+                                   (is-result
+                                     (pass-to-next-stage)))))))
