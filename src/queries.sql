dd7668f7a865ea45eb0d1b561ae8c4d2484fd863
Greg Poirier <greg@opsee.com>
2016-01-20 19:52:27 -0800

    Refer to bartnet_assertions

--- src/queries.sql
+++ src/queries.sql
@@ -1,25 +1,25 @@
 --name: insert-into-assertions<!
 -- Inserts a new record into the assertions table.
-insert into assertions (check_id, customer_id, relationship, "key", "value", operand) values
+insert into bartnet_assertions (check_id, customer_id, relationship, "key", "value", operand) values
         (:check_id, :customer_id::uuid, :relationship::relationship_type, :key, :value, :operand);
 
 --name: get-assertions-by-check-and-customer
 -- Retrieves assertions by the check_id and customer_id.
-select * from assertions where check_id=:check_id and customer_id=:customer_id::uuid;
+select * from bartnet_assertions where check_id=:check_id and customer_id=:customer_id::uuid;
 
 --name: get-assertions-by-customer
 -- Retrieves all of the assertions for a customer_id.
-select * from assertions where customer_id=:customer_id::uuid;
+select * from bartnet_assertions where customer_id=:customer_id::uuid;
 
 --name: get-assertions-by-check
-select * from assertions where check_id=:check_id;
+select * from bartnet_assertions where check_id=:check_id;
 
 --name: delete-assertions-by-check-and-customer!
 -- Deletes an assertion.
-delete from assertions where check_id=:check_id and customer_id=:customer_id::uuid;
+delete from bartnet_assertions where check_id=:check_id and customer_id=:customer_id::uuid;
 
 --name: get-assertions
-select * from assertions;
+select * from bartnet_assertions;
 
 ----------------------------------------------
 
