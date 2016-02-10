b4685787b95fcf07141142574ab72ff3e9c5756a
Greg Poirier <greg@opsee.com>
2016-02-09 19:10:16 -0800

    Fix tests and verify sqs/handle-event is getting called

--- test/beavis/t_kundenbenachrichtigung.clj
+++ test/beavis/t_kundenbenachrichtigung.clj
@@ -8,7 +8,7 @@
             [clojure.tools.logging :as log]
             [opsee.middleware.config :refer [config]]
             [opsee.middleware.protobuilder :as proto]
-            [beavis.alerts.email :as email]))
+            [beavis.alerts.sqs :as sqs]))
 
 (def cliff-id "154ba57a-5188-11e5-8067-9b5f2d96dce1")
 (def greg-id "154ba57a-5188-11e5-8067-9b5f2d96dce2")
@@ -19,6 +19,8 @@
   (assoc (proto/proto->hash (check-result 3 3 0)) :customer_id customer_id :check_id check_id :check_name check_id :passing true))
 (defn failing-event [customer_id check_id]
   (assoc (proto/proto->hash (check-result 3 1 0)) :customer_id customer_id :check_id check_id :check_name check_id :passing false))
+
+(def sqs-event (atom nil))
 
 (defn next-fn [event]
   event)
@@ -37,11 +39,11 @@
 
 (facts
   "sending alerts"
-  (with-redefs [email/handle-event (fn [event notification] nil)]
-    (with-state-changes [(before :facts (doto
-                                          (do-setup)
-                                          notification-fixtures
-                                          alert-fixtures))]
+  (with-redefs [sqs/handle-event (fn [event] (reset! sqs-event event))]
+    (with-state-changes [(before :facts (do
+                                          (doto (do-setup)
+                                          alert-fixtures)
+                                          (reset! sqs-event nil)))]
                         (fact "creates an alert if no previous alert and failing event"
                               (let [stage (k/alert-stage @db test-config)
                                     event (failing-event greg-id unseen-check-id)]
@@ -51,7 +53,8 @@
                                                                               :check_id    unseen-check-id}))]
                                   (:state alert) => "open"
                                   (str (:customer_id alert)) => greg-id
-                                  (:check_id alert) => unseen-check-id)
+                                  (:check_id alert) => unseen-check-id
+                                  @sqs-event => event)
                                 (s/stop-stage! stage)))
                         (fact "creates an alert if previous alert was resolved"
                               (let [stage (k/alert-stage @db test-config)
@@ -62,7 +65,8 @@
                                                                               :check_id    resolved-alert-check}))]
                                   (:state alert) => "open"
                                   (str (:customer_id alert)) => cliff-id
-                                  (:check_id alert) => resolved-alert-check)
+                                  (:check_id alert) => resolved-alert-check
+                                  @sqs-event => event)
                                 (s/stop-stage! stage)))
                         (fact "resolves an alert if passing event and previous open alert"
                               (let [stage (k/alert-stage @db test-config)
@@ -73,5 +77,6 @@
                                                                               :check_id    open-alert-check}))]
                                   (:state alert) => "resolved"
                                   (str (:customer_id alert)) => cliff-id
-                                  (:check_id alert) => open-alert-check)
+                                  (:check_id alert) => open-alert-check
+                                  @sqs-event => event)
                                 (s/stop-stage! stage))))))
